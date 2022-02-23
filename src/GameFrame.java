5c786d954678c884d84e284f77c0a0f547bca9a0
healyr4 <healyr4@tcd.ie>
2022-02-23 21:04:55 +0000

    Added to GameFrame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,13 +1,18 @@
 import javax.swing.JFrame;
 
 /**
- * 
- */
-
-/**
  * @author healy
  *
  */
 public class GameFrame extends JFrame{
-
+	GameFrame(){
+		this.add(new GamePanel());
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		// Fit Jframe around components we add to frame
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
 }
